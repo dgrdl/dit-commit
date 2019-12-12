321c3a88ba3f0ff10667df3dcdff91d651f1bf76
Qakulukiam <Qakulukiam@users.noreply.github.com>
2019-12-13 00:00:55 +0100

    .- -.. -.. / .--. .-. --- -- .--. - ... / .- -. -.. / --. .. - / -.. .. .-. / ... . .- .-. -.-. .... / ..-. --- .-. / .... --- --- -.- / -.-. --- -- -- .- -. -..

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 #!/usr/bin/env node
 
-const encode = require('./morse').encode;
+const { encode } = require('./morse');
+const { findGitRoot } = require('./git');
 const yargs = require('yargs');
 const simpleGit = require('simple-git')();
+const chalk = require('chalk');
+const inquirer = require('inquirer');
+
+// IDEAS:
+// - consider adding an 'encode' command that just prints the morse code to your terminal (or wherever)
+
+// TODO: add flag / option to remove hook again
+// TODO: add a check to prevent double encoding (to both the hook and the commit commands)
 
 yargs
   .parserConfiguration({
@@ -40,6 +49,71 @@ yargs
       });
 
       simpleGit.commit(message, [], args);
+    }
+  })
+  .command({
+    command: 'hook',
+    desc:
+      "Adds a git hook to the nearest repository that automatically converts all commit messages into morse code.\n\nYes, it's a terrible idea.",
+    builder: {
+      global: {
+        alias: 'g',
+        describe:
+          'Add the hook to your global git settings instead.\n\nPlease never use this flag.'
+      }
+    },
+    handler: argv => {
+      inquirer
+        .prompt({
+          type: 'confirm',
+          name: 'confirmation',
+          message:
+            'This will install a hook which automatically converts your commit messages in this repository into Morse code. Are you sure that you want this?',
+          default: false
+        })
+        .then(({ confirmation }) => {
+          if (!confirmation) {
+            console.log(chalk.gray('Good choice...'));
+            return;
+          }
+
+          inquirer
+            .prompt({
+              type: 'input',
+              name: 'morseConfirmation',
+              message:
+                'WARNING: This is a really bad idea. If you are still sure that you want this to happen, type the word YES in Morse code.'
+            })
+            .then(({ morseConfirmation }) => {
+              // -.-- . ...
+              if (encode('yes').trim() !== morseConfirmation.trim()) {
+                console.log(
+                  chalk.red(
+                    'Wrong input! Your commits live to see another day.'
+                  )
+                );
+                return;
+              }
+
+              if (argv.global) {
+                console.log('omg global...');
+              } else {
+                const gitRoot = findGitRoot();
+                if (!gitRoot) {
+                  console.warn(
+                    chalk.yellow('Could not create hook: No git project found!')
+                  );
+                  return;
+                }
+
+                // TODO: add hook (symlink?) to git repo
+              }
+
+              console.log(
+                chalk.green("Git hook created. Don't say we didn't warn you.")
+              );
+            });
+        });
     }
   })
   .help().argv;
